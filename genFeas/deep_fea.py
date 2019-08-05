7d71b4a3b387bb3f145e1b556d63e825190bec4e
PingjunChen <chenpingjun@gmx.com>
2019-08-05 16:16:07 -0400

    update main.py to deep_fea.py

--- genFeas/deep_fea.py
+++ genFeas/deep_fea.py
@@ -6,7 +6,7 @@ import numpy as np
 import torch
 import torch.backends.cudnn as cudnn
 
-from fea_gen import gen_features
+from fea_gen import gen_deep_features
 
 
 def set_args():
@@ -14,13 +14,14 @@ def set_args():
     parser.add_argument('--seed',            type=int,   default=1234)
     parser.add_argument('--batch_size',      type=int,   default=64)
     parser.add_argument('--patch_size',      type=int,   default=256)
-    parser.add_argument('--model_dir',       type=str,   default="../data/ThyroidS5/Models/PatchL2Models")
+    parser.add_argument('--model_dir',       type=str,   default="../data/ThyroidS6/Models/PatchL2Models")
     parser.add_argument('--model_name',      type=str,   default="resnet50")
-    parser.add_argument('--model_path',      type=str,   default="Thyroid-ft-0.883.pth")
+    parser.add_argument('--model_path',      type=str,   default="Thyroid03-0.9213.pth")
 
-    parser.add_argument('--roi_dir',         type=str,   default="../data/ThyroidS5/ImgsROI")
-    parser.add_argument('--data_mode',       type=str,   default="val")
-    parser.add_argument('--fea_dir',         type=str,   default="../data/ThyroidS5/FeasROI/L2Feas")
+    parser.add_argument('--roi_dir',         type=str,   default="../data/ThyroidS6/ImgsROI")
+    parser.add_argument('--data_mode',       type=str,   default="train")
+    parser.add_argument('--fea_dir',         type=str,   default="../data/ThyroidS6/FeasROI/L2Feas")
+    parser.add_argument('--device_id',       type=str,   default="1")
 
     args = parser.parse_args()
     return args
@@ -28,9 +29,7 @@ def set_args():
 
 if __name__ == '__main__':
     args = set_args()
-    os.environ["CUDA_VISIBLE_DEVICES"] = "2"
-    torch.cuda.manual_seed(args.seed)
-    cudnn.benchmark = True
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.device_id
 
     model_full_path = os.path.join(args.model_dir, args.model_name, args.model_path)
     ft_model = torch.load(model_full_path)
