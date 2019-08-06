242fc75b76e3a78b9bf453cb425e1559cd5ed851
PingjunChen <chenpingjun@gmx.com>
2019-08-06 15:35:53 -0400

    add fea generation

--- genFeas/deep_fea.py
+++ genFeas/deep_fea.py
@@ -5,23 +5,21 @@ import argparse
 import numpy as np
 import torch
 import torch.backends.cudnn as cudnn
-
 from fea_gen import gen_deep_features
 
 
 def set_args():
     parser = argparse.ArgumentParser(description='Thyroid Classification')
     parser.add_argument('--seed',            type=int,   default=1234)
-    parser.add_argument('--batch_size',      type=int,   default=64)
+    parser.add_argument('--batch_size',      type=int,   default=16)
     parser.add_argument('--patch_size',      type=int,   default=256)
-    parser.add_argument('--model_dir',       type=str,   default="../data/ThyroidS6/Models/PatchL2Models")
-    parser.add_argument('--model_name',      type=str,   default="resnet50")
-    parser.add_argument('--model_path',      type=str,   default="Thyroid03-0.9213.pth")
-
-    parser.add_argument('--roi_dir',         type=str,   default="../data/ThyroidS6/ImgsROI")
+    parser.add_argument('--model_dir',       type=str,   default="../data/ThyroidS2/Models/PatchL2Models")
+    parser.add_argument('--roi_dir',         type=str,   default="../data/ThyroidS2/ImgsROI")
+    parser.add_argument('--fea_dir',         type=str,   default="../data/ThyroidS2/FeasROI/L2Feas")
+    parser.add_argument('--model_name',      type=str,   default="vgg16bn")
+    parser.add_argument('--model_path',      type=str,   default="Thyroid01-0.8986.pth")
     parser.add_argument('--data_mode',       type=str,   default="train")
-    parser.add_argument('--fea_dir',         type=str,   default="../data/ThyroidS6/FeasROI/L2Feas")
-    parser.add_argument('--device_id',       type=str,   default="1")
+    parser.add_argument('--device_id',       type=str,   default="7")
 
     args = parser.parse_args()
     return args
@@ -36,4 +34,4 @@ if __name__ == '__main__':
     ft_model.cuda()
 
     print("Starting feature generation...")
-    gen_features(args.roi_dir, args.fea_dir, args.data_mode, ft_model, args)
+    gen_deep_features(args.roi_dir, args.fea_dir, args.data_mode, ft_model, args)
--- genFeas/gist_fea.py
+++ genFeas/gist_fea.py
@@ -10,11 +10,11 @@ def set_args():
     parser = argparse.ArgumentParser(description='Gist feature extraction')
     parser.add_argument('--seed',            type=int,   default=1234)
     parser.add_argument('--patch_size',      type=int,   default=256)
-    parser.add_argument('--model_name',        type=str,   default="gist")
+    parser.add_argument('--model_name',      type=str,   default="gist")
 
-    parser.add_argument('--roi_dir',         type=str,   default="../data/ThyroidS6/ImgsROI")
+    parser.add_argument('--roi_dir',         type=str,   default="../data/ThyroidS5/ImgsROI")
     parser.add_argument('--data_mode',       type=str,   default="val")
-    parser.add_argument('--fea_dir',         type=str,   default="../data/ThyroidS6/FeasROI/L2Feas")
+    parser.add_argument('--fea_dir',         type=str,   default="../data/ThyroidS5/FeasROI/L2Feas")
 
     args = parser.parse_args()
     return args
