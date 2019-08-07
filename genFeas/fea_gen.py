cabad5d84abd714b99ea3a3b07a72b8afacc6b47
PingjunChen <chenpingjun@gmx.com>
2019-08-06 21:08:51 -0400

    add inceptionv3 feature extraction

--- genFeas/fea_gen.py
+++ genFeas/fea_gen.py
@@ -41,7 +41,7 @@ class PatchDataset(data.Dataset):
         self.rgb_mean = (0.800, 0.600, 0.800)
         self.rgb_std = (0.125, 0.172, 0.100)
         self.transform = transforms.Compose([transforms.ToPILImage(),
-            transforms.Resize((224, 224)), transforms.ToTensor(),
+            transforms.Resize((299, 299)), transforms.ToTensor(),
             transforms.Normalize(mean=self.rgb_mean, std=self.rgb_std)])
 
     def __len__(self):
@@ -78,6 +78,58 @@ def extract_deep_feas(model, x, model_name):
         x = torch.flatten(x, 1)
         fea = model.classifier[:4](x)
         logit = model.classifier[4:](fea)
+        prob = F.softmax(logit, dim=-1)
+    elif "inception" in model_name:
+        if model.transform_input:
+            x_ch0 = torch.unsqueeze(x[:, 0], 1) * (0.229 / 0.5) + (0.485 - 0.5) / 0.5
+            x_ch1 = torch.unsqueeze(x[:, 1], 1) * (0.224 / 0.5) + (0.456 - 0.5) / 0.5
+            x_ch2 = torch.unsqueeze(x[:, 2], 1) * (0.225 / 0.5) + (0.406 - 0.5) / 0.5
+            x = torch.cat((x_ch0, x_ch1, x_ch2), 1)
+        # N x 3 x 299 x 299
+        x = model.Conv2d_1a_3x3(x)
+        # N x 32 x 149 x 149
+        x = model.Conv2d_2a_3x3(x)
+        # N x 32 x 147 x 147
+        x = model.Conv2d_2b_3x3(x)
+        # N x 64 x 147 x 147
+        x = F.max_pool2d(x, kernel_size=3, stride=2)
+        # N x 64 x 73 x 73
+        x = model.Conv2d_3b_1x1(x)
+        # N x 80 x 73 x 73
+        x = model.Conv2d_4a_3x3(x)
+        # N x 192 x 71 x 71
+        x = F.max_pool2d(x, kernel_size=3, stride=2)
+        # N x 192 x 35 x 35
+        x = model.Mixed_5b(x)
+        # N x 256 x 35 x 35
+        x = model.Mixed_5c(x)
+        # N x 288 x 35 x 35
+        x = model.Mixed_5d(x)
+        # N x 288 x 35 x 35
+        x = model.Mixed_6a(x)
+        # N x 768 x 17 x 17
+        x = model.Mixed_6b(x)
+        # N x 768 x 17 x 17
+        x = model.Mixed_6c(x)
+        # N x 768 x 17 x 17
+        x = model.Mixed_6d(x)
+        # N x 768 x 17 x 17
+        x = model.Mixed_6e(x)
+        # N x 768 x 17 x 17
+        x = model.Mixed_7a(x)
+        # N x 1280 x 8 x 8
+        x = model.Mixed_7b(x)
+        # N x 2048 x 8 x 8
+        x = model.Mixed_7c(x)
+        # N x 2048 x 8 x 8
+        # Adaptive average pooling
+        x = F.adaptive_avg_pool2d(x, (1, 1))
+        # N x 2048 x 1 x 1
+        x = F.dropout(x, training=model.training)
+        # N x 2048 x 1 x 1
+        fea = torch.flatten(x, 1)
+        # N x 2048
+        logit = model.fc(fea)
         prob = F.softmax(logit, dim=-1)
     else:
         raise AssertionError("Unknown model name {}".format(model_name))
